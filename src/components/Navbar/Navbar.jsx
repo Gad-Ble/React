e4dec714ff8fd1d9771bb48ee8a26e907ae3955d
Gad-Ble <gad26166@gmai.com>
2024-12-18 17:56:12 +0500

    refactor class navlink Lesson 20

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,29 +5,19 @@ import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return <nav className={classes.nav}>
         <div className={classes.item}>
-            <NavLink className={({ isActive }) =>
-                isActive ? `${classes.link} ${classes.active}` : classes.link
-            } to="/profile">Profile</NavLink>
+            <NavLink className={({ isActive }) => isActive && `${classes.active}`} to="/profile">Profile</NavLink>
         </div>
         <div className={classes.item}>
-            <NavLink to="/dialogs" className={({ isActive }) =>
-                isActive ? `${classes.link} ${classes.active}` : classes.link
-            }>Messages</NavLink>
+            <NavLink to="/dialogs" className={({ isActive }) => isActive && `${classes.active}`}>Messages</NavLink>
         </div>
         <div className={classes.item}>
-            <NavLink to="/news" className={({ isActive }) =>
-                isActive ? `${classes.link} ${classes.active}` : classes.link
-            }>News</NavLink>
+            <NavLink to="/news" className={({ isActive }) => isActive && `${classes.active}`}>News</NavLink>
         </div>
         <div className={classes.item}>
-            <NavLink to="/music" className={({ isActive }) =>
-                isActive ? `${classes.link} ${classes.active}` : classes.link
-            }>Music</NavLink>
+            <NavLink to="/music" className={({ isActive }) => isActive && `${classes.active}`}>Music</NavLink>
         </div>
         <div className={classes.item}>
-            <NavLink to="/settings" className={({ isActive}) =>
-                isActive ? `${classes.active}` : classes.link
-            }   >Settings</NavLink>
+            <NavLink to="/settings" className={({ isActive }) => isActive && `${classes.active}`}>Settings</NavLink>
         </div>
     </nav>
 }
