ba3bf67d9a47bd0b176caadd7bb5806959adcfb9
Gad-Ble <gad26166@gmail.com>
2024-12-21 02:15:49 +0500

    Lesson 25

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -22,14 +22,14 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Kostya'},
         {id: 2, name: 'Sveta'},
         {id: 3, name: 'Pasha'},
         {id: 4, name: 'Maksim'},
         {id: 5, name: 'Lexa'}
     ];
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'Hi, how are you?'},
         {id: 3, message: 'Good'},
@@ -37,21 +37,16 @@ const Dialogs = (props) => {
         {id: 5, message: 'Yo'}
     ];
 
+    let dialogElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messageElements = messages.map(m => <Message message={m.message}/>);
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                {dialogElements}
             </div>
             <div className={classes.messages}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message}/>
-                <Message message={messagesData[2].message}/>
-                <Message message={messagesData[3].message}/>
-                <Message message={messagesData[4].message}/>
+                {messageElements}
             </div>
         </div>
 )
--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -3,34 +3,31 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
-  let postsData = [
-    {id: 1, message: 'Hi, how are you', likesCount: 1},
-    {id: 2, message: 'It\'s my first post', likesCount: 12},
-    {id: 3, message: 'Good', likesCount: 11},
-    {id: 4, message: 'Yo', likesCount: 18},
-    {id: 5, message: 'Yo', likesCount: 3}
-  ];
+    let postsData = [
+        {id: 1, message: 'Hi, how are you', likesCount: 1},
+        {id: 2, message: 'It\'s my first post', likesCount: 12},
+        {id: 3, message: 'Good', likesCount: 11},
+        {id: 4, message: 'Yo', likesCount: 18},
+        {id: 5, message: 'Yo', likesCount: 3}
+    ];
+    let posts = postsData.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
-  return <div className={classes.postsBlock}>
-    <div>
-      <h3>My posts</h3>
-      <div>
+    return <div className={classes.postsBlock}>
         <div>
-          <textarea></textarea>
+            <h3>My posts</h3>
+            <div>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div className={classes.buttons}>
+                    <button>Add</button>
+                </div>
+            </div>
+            <div className={classes.posts}>
+                {posts}
+            </div>
         </div>
-        <div className={classes.buttons}>
-          <button>Add</button>
-        </div>
-      </div>
-      <div className={classes.posts}>
-        <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-        <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
-        <Post message={postsData[2].message} likesCount={postsData[2].likesCount}/>
-        <Post message={postsData[3].message} likesCount={postsData[3].likesCount}/>
-        <Post message={postsData[4].message} likesCount={postsData[4].likesCount}/>
-      </div>
     </div>
-  </div>
 }
 
 export default MyPosts;
